e10c40b93de5d64c90dec9650ca1719e74d63967
Denys Kuts <dena.kuts1@gmail.com>
2018-02-05 20:17:37 +0200

    initial commit

---
+++ app/src/main/java/com/chnumarks/MainActivity.kt
@@ -0,0 +1,52 @@
+package com.chnumarks
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+
+class MainActivity : AppCompatActivity() {
+
+    val textView by lazy {
+        findViewById<TextView>(R.id.textView)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val user = FirebaseAuth.getInstance().currentUser
+
+        if(user == null){
+            val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                    .requestIdToken(getString(R.string.web_client_id))
+                    .requestEmail()
+                    .build()
+            val signInIntent = GoogleSignIn.getClient(this, gso).signInIntent
+            startActivityForResult(signInIntent, 1)
+        } else {
+            textView.text = user.email
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode == 1){
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                val account = task.getResult(ApiException::class.java)
+                textView.text = account.email
+            }catch (e: ApiException){
+                textView.text = "exception"
+            }
+        }
+    }
+}
