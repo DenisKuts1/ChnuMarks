94c6d287a790d79e0e6c9e94ee18af5b60547341
Denys Kuts <dena.kuts1@gmail.com>
2018-02-06 23:21:16 +0200

    test authentication

--- app/src/main/java/com/chnumarks/MainActivity.kt
+++ app/src/main/java/com/chnumarks/MainActivity.kt
@@ -9,9 +9,11 @@ import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.OnCompleteListener
 import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.GoogleAuthProvider
 
 class MainActivity : AppCompatActivity() {
 
@@ -19,13 +21,17 @@ class MainActivity : AppCompatActivity() {
         findViewById<TextView>(R.id.textView)
     }
 
+    /*val mAuth by lazy {
+        FirebaseAuth.getInstance()
+    }*/
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val user = FirebaseAuth.getInstance().currentUser
 
-        if(user == null){
+        if (user == null) {
             val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
                     .requestIdToken(getString(R.string.web_client_id))
                     .requestEmail()
@@ -39,14 +45,29 @@ class MainActivity : AppCompatActivity() {
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        if(requestCode == 1){
+        var s = ""
+        if (requestCode == 1) {
             val task = GoogleSignIn.getSignedInAccountFromIntent(data)
             try {
                 val account = task.getResult(ApiException::class.java)
-                textView.text = account.email
-            }catch (e: ApiException){
-                textView.text = "exception"
+                val credential = GoogleAuthProvider.getCredential(account.idToken, null)
+                s += account.displayName + " " + account.email
+                FirebaseAuth.getInstance().signInWithCredential(credential)
+                        .addOnCompleteListener(this, { t ->
+                            run {
+                                if (t.isSuccessful) {
+                                    val user = FirebaseAuth.getInstance().currentUser
+                                    s += user!!.email ?: ""
+                                } else {
+                                    s += "Uh oh..."
+                                }
+                                textView.text = t.exception!!.message
+                            }
+                        })
+            } catch (e: ApiException) {
+                s += e.localizedMessage
             }
+            textView.text = s
         }
     }
 }
