8a08e04a3369ce5eee1daeb57291552f539ee93b
Denys Kuts <dena.kuts1@gmail.com>
2018-02-08 01:54:45 +0200

    simple navigation drawer

--- app/src/main/java/com/chnumarks/MainActivity.kt
+++ app/src/main/java/com/chnumarks/MainActivity.kt
@@ -3,6 +3,9 @@ package com.chnumarks
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.Toolbar
+import android.view.Menu
+import android.widget.ImageView
 import android.widget.TextView
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
@@ -14,43 +17,56 @@ import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.GoogleAuthProvider
+import com.squareup.picasso.Picasso
 
 class MainActivity : AppCompatActivity() {
 
     val textView by lazy {
         findViewById<TextView>(R.id.textView)
     }
+    val toolbar by lazy {
+        findViewById<Toolbar>(R.id.main_toolbar)
+    }
 
     /*val mAuth by lazy {
         FirebaseAuth.getInstance()
     }*/
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        //menuInflater.inflate(R.menu.toolbar_menu, menu)
+        //textView.text = "Hello"
+        return true
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val user = FirebaseAuth.getInstance().currentUser
+        setSupportActionBar(toolbar)
+        supportActionBar!!.setDisplayShowTitleEnabled(false)
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.web_client_id))
+                .requestEmail()
+                .build()
+        val signInIntent = GoogleSignIn.getClient(this, gso).signInIntent
+        startActivityForResult(signInIntent, 1)
+        /*val user = FirebaseAuth.getInstance().currentUser
 
         if (user == null) {
-            val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                    .requestIdToken(getString(R.string.web_client_id))
-                    .requestEmail()
-                    .build()
-            val signInIntent = GoogleSignIn.getClient(this, gso).signInIntent
-            startActivityForResult(signInIntent, 1)
+
         } else {
             textView.text = user.email
-        }
+        }*/
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        var s = ""
         if (requestCode == 1) {
             val task = GoogleSignIn.getSignedInAccountFromIntent(data)
             try {
                 val account = task.getResult(ApiException::class.java)
-                val credential = GoogleAuthProvider.getCredential(account.idToken, null)
+
+
+                /*val credential = GoogleAuthProvider.getCredential(account.idToken, null)
                 s += account.displayName + " " + account.email
                 FirebaseAuth.getInstance().signInWithCredential(credential)
                         .addOnCompleteListener(this, { t ->
@@ -63,11 +79,12 @@ class MainActivity : AppCompatActivity() {
                                 }
                                 textView.text = t.exception!!.message
                             }
-                        })
+                        })*/
             } catch (e: ApiException) {
-                s += e.localizedMessage
+                //s += e.localizedMessage
+                textView.text = "Ops..."
             }
-            textView.text = s
+            //textView.text = s
         }
     }
 }
