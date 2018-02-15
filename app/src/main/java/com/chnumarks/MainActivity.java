340f081aa450417f35de4ea6793de627d5db2476
Denys Kuts <dena.kuts1@gmail.com>
2018-02-15 19:04:36 +0200

    changed MainActivity to java

---
+++ app/src/main/java/com/chnumarks/MainActivity.java
@@ -0,0 +1,104 @@
+package com.chnumarks;
+
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.WindowManager;
+
+import com.chnumarks.R;
+import com.chnumarks.UtilsKt;
+import com.chnumarks.fragments.EditFragment;
+import com.chnumarks.fragments.NavigationDrawerFragment;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+
+/**
+ * Created by denak on 15.02.2018.
+ */
+
+public class MainActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+    private FirebaseAuth auth;
+    private DrawerLayout drawerLayout;
+    private TabLayout tabLayout;
+    private NavigationDrawerFragment navigationFragment;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        toolbar = findViewById(R.id.main_toolbar);
+        drawerLayout = findViewById(R.id.main_drawer_layout);
+        tabLayout = findViewById(R.id.edit_tab_layout);
+        UtilsKt.setLightStatusBar(toolbar, this);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+            int offset = (int) (25 * getResources().getDisplayMetrics().density + 0.5f);
+            toolbar.setPadding(0, offset, 0, 0);
+        }
+        navigationFragment = new NavigationDrawerFragment();
+        navigationFragment.setParameters(toolbar, drawerLayout);
+        getSupportFragmentManager().beginTransaction().add(R.id.main_drawer_layout, navigationFragment).commit();
+
+        EditFragment editFragment = new EditFragment();
+        editFragment.setUpTabLayout(tabLayout);
+        getSupportFragmentManager().beginTransaction().add(R.id.main_content, editFragment).commit();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        auth = FirebaseAuth.getInstance();
+        FirebaseUser user = auth.getCurrentUser();
+        if (user == null) {
+            GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                    .requestIdToken(getString(R.string.web_client_id))
+                    .requestEmail()
+                    .build();
+            Intent signInIntent = GoogleSignIn.getClient(this, gso).getSignInIntent();
+            startActivityForResult(signInIntent, 1);
+        } else {
+            updateUI(user);
+        }
+    }
+
+    private void updateUI(FirebaseUser user) {
+        navigationFragment.setUser(user);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1) {
+            Task task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                GoogleSignInAccount account = (GoogleSignInAccount) task.getResult(ApiException.class);
+                AuthCredential credential = GoogleAuthProvider.getCredential(account.getIdToken(), null);
+                auth.signInWithCredential(credential).addOnCompleteListener(task1 -> {
+                    FirebaseUser newUser = auth.getCurrentUser();
+                    updateUI(newUser);
+                });
+            } catch (Throwable e) {
+
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/chnumarks/fragments/NavigationDrawerFragment.kt
@@ -0,0 +1,72 @@
+package com.chnumarks.fragments
+
+import android.os.Build
+import android.os.Bundle
+import android.support.design.widget.NavigationView
+import android.support.v4.app.Fragment
+import android.support.v4.widget.DrawerLayout
+import android.support.v7.widget.Toolbar
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import com.chnumarks.R
+import com.chnumarks.clearLightStatusBar
+import com.chnumarks.setLightStatusBar
+import com.google.firebase.auth.FirebaseUser
+import com.squareup.picasso.Picasso
+import de.hdodenhof.circleimageview.CircleImageView
+
+/**
+ * Created by denak on 15.02.2018.
+ */
+class NavigationDrawerFragment : Fragment(), DrawerLayout.DrawerListener {
+
+    private lateinit var toolbar: Toolbar
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var navigationView: NavigationView
+
+    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val view = inflater!!.inflate(R.layout.navigation_drawer_fragment, container, false)
+        drawerLayout.addDrawerListener(this)
+        navigationView = view.findViewById(R.id.navigation_view)
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+            val offset = (25 * resources.displayMetrics.density + 0.5f).toInt()
+            navigationView.getHeaderView(0).setPadding(0, offset,0,0)
+        }
+        return view
+    }
+
+    fun setParameters(toolbar: Toolbar, drawerLayout: DrawerLayout) {
+        this.toolbar = toolbar
+        this.drawerLayout = drawerLayout
+    }
+
+    fun setUser(user: FirebaseUser?){
+        if (user != null) {
+            val profileName = navigationView.getHeaderView(0).findViewById<TextView>(R.id.navigation_header_profile_name)
+            val profileEmail = navigationView.getHeaderView(0).findViewById<TextView>(R.id.navigation_header_profile_email)
+            val profileImage = navigationView.getHeaderView(0).findViewById<CircleImageView>(R.id.navigation_header_profile_image)
+            profileName.text = user.displayName
+            profileEmail.text = user.email
+            Picasso.with(activity).load(user.photoUrl).into(profileImage)
+        }
+    }
+
+    override fun onDrawerStateChanged(newState: Int) {}
+
+    override fun onDrawerSlide(drawerView: View?, slideOffset: Float) {}
+
+    override fun onDrawerClosed(drawerView: View?) {
+        if (drawerView != null) {
+            setLightStatusBar(toolbar, activity)
+        }
+    }
+
+    override fun onDrawerOpened(drawerView: View?) {
+        if (drawerView != null) {
+            clearLightStatusBar(toolbar, activity)
+        }
+    }
+
+}
